2f80eb7a89fc9e8eaeadc0527c20a6c8372dba73
brendaaurelie <brenda.aurelie@gmail.com>
2021-07-25 12:47:32 +0700

    make campaign details dynamic

--- src/app/single-card/single-card.component.ts
+++ src/app/single-card/single-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router'
+import {Campaign, CAMPAIGNS} from '../campaigns';
 
 @Component({
   selector: 'app-single-card',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./single-card.component.css']
 })
 export class SingleCardComponent implements OnInit {
+  campaign : Campaign|undefined;
+  constructor(private route: ActivatedRoute,) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+ 
+    ngOnInit() {
+      // First get the product id from the current route.
+      const routeParams = this.route.snapshot.paramMap;
+      const campaignIdFromRoute = Number(routeParams.get('campaignId'));
+    
+      // Find the product that correspond with the id provided in route.
+      this.campaign = CAMPAIGNS.find(campaign => campaign.id === campaignIdFromRoute);
+    }
+  
 
 }

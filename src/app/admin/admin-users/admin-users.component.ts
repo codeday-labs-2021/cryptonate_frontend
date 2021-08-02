7273916d173003051978e0f1d205e96ad2cf1325
Ken Wu <wuq@asmilan.org>
2021-08-02 18:17:43 +0200

    Add list of campaigns and users in admin pages

--- src/app/admin/admin-users/admin-users.component.ts
+++ src/app/admin/admin-users/admin-users.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/_services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-users',
   templateUrl: './admin-users.component.html',
   styleUrls: ['./admin-users.component.css']
 })
-export class AdminUsersComponent implements OnInit {
 
-  constructor() { }
+export class AdminUsersComponent implements OnInit {
+  constructor(private _userService: UserService,
+    private router:Router) { }
+  
+  users = [];
+  total_users : number;
 
   ngOnInit(): void {
+    //call backend APIs 
+    // save into campaigns and display campaigns 
+
+    this._userService.getUser()
+      .subscribe(users => {
+        this.users = users;
+        this.total_users = Object.keys(users).length
+      });
   }
 
 }
